1bb9aeb4ca5f40abc1a2f44a446d8e12f7bb499d
NicoCatta <nicolacattaneo02@gmail.com>
2023-12-22 09:36:19 +0100

    db

---
+++ GestioneSaleOspedale/GestioneSale_logic/src/model/DataService.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import GestioneSale.model.generated.tables.*;
+import GestioneSale.db_sqlite.CreateDB;
+
+
+public class DataService {
+	
+	private Connection conn;
+	
+	private DSLContext create;
+	
+	
+	public DataService() {
+		
+		try {
+			conn = DriverManager.getConnection(CreateDB.DB_URL);
+			create = DSL.using(conn, SQLDialect.SQLITE);
+		}
+		catch (SQLException e) {
+		   System.out.println("ERROR: failed to connect!");
+		   System.out.println("ERROR: " + e.getErrorCode());
+		   e.printStackTrace();
+		}
+	}
+	
+	//Aggiungere qui sotto le query in Java
+	
+	
+	
+
+}
