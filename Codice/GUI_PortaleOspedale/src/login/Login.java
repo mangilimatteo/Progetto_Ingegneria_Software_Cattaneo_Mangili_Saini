ae1fc0f4daede3a882168d2fa7c78ec454d67b13
mangilimatteo <m.mangili15@studenti.unibg.it>
2024-03-09 16:21:32 +0100

    prove testing

--- Codice/GUI_PortaleOspedale/src/login/Login.java
+++ Codice/GUI_PortaleOspedale/src/login/Login.java
@@ -3,6 +3,7 @@ package login;
 
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
--- Codice/GestioneSale_business_logic/src/model/GenerateCode.java
+++ Codice/GestioneSale_business_logic/src/model/GenerateCode.java
@@ -21,3 +21,4 @@ public class GenerateCode {
 		GenerationTool.generate(configuration); 
 	}
 } 
+
